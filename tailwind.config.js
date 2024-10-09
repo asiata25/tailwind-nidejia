065417550906cf312fe2b2213c71ab306a433d7f
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-10-09 14:47:10 +0700

    design: navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,26 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       fontFamily: {
         sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        brand: {
+          primary: "#FF9357",
+          dark: "#141414",
+          grey: "#A8A8A8",
+        },
+      },
+      spacing: {
+        7.5: "30px",
+      },
+      maxWidth: {
+        1130: '1130px'
+      }
     },
   },
   plugins: [],
